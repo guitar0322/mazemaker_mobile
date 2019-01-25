9f8b31ce68767e754eb4f67401f530969d88e8af
Lee Dong Gil <donggillee94@gmail.com>
2019-01-25 18:53:00 +0900

    initial commit

--- router/match/match_complete.js
+++ router/match/match_complete.js
@@ -1,6 +1,39 @@
 var router = require('express').Router();
 var conn = require('../../config/db');
 
+var map = new Array();
+var wall = new Array();
+for(var i = 0; i < 18; i++) {
+  wall[i] = new Array(18);
+}
+var Random = (min, max) => {
+  var ranNum = Math.floor(Math.random()*(max-min+1)) + min;
+  return ranNum;
+}
+var create_map = (map) => {
+  map[0] = Random(8,25);
+  map[1] = Random(0,2);
+  return map;
+}
+var create_wall = (wall) => {
+  var tmp = Random(8, 14);
+
+  for(var i = 0; i < tmp; i++) {
+    var x = Random(3, 16);
+    var y = Random(1, 16);
+
+    if(wall[x][y] === 1 || wall[x][y+1] === 1 || wall[x-1][y] === 1 || wall[x-1][y+1] === 1) {
+      i--;
+    } else {
+      wall[x][y] = 1;
+      wall[x][y+1] = 1;
+      wall[x-1][y] = 1;
+      wall[x-1][y+1] = 1;
+    }
+  }
+  return wall;
+}
+
 router.post('/', (req, res) => {
   var accpet = req.body.accept;
   var nickname = req.body.nickname;
@@ -18,7 +51,9 @@ router.post('/', (req, res) => {
               if(err) throw err;
               else {
                 if(result.length === 8) {
-                  var msg = {"status":"OK", "result":result};
+                  var wall = create_wall(wall);
+                  var map = create_map(map);
+                  var msg = {"status":"OK", "result":result, "wall":wall, "map":map};
                   return res.json(msg);
                 }
               }
