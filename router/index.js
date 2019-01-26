1cb0dc0474c46f815cd4530a99acbace32c117a0
minwoo <zox2323@naver.com>
2019-01-26 17:31:30 +0900

    index.js

--- router/index.js
+++ router/index.js
@@ -5,4 +5,14 @@ var rank = require('./rank/index')
 router.use('/register',register);
 router.use('/login',login);
 router.use('/rank',rank);
+var match = require('./match/match');
+var match_complete = require('./match/match_complete');
+var match_cancel = require('./match/match_cancel');
+
+router.use('/match', match);
+router.use('/register',register);
+router.use('/login',login);
+router.use('/match/cancel', match_cancel);
+router.use('/match/complete', match_complete);
+
 module.exports = router;
