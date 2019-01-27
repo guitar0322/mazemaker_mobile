2eb9ce82ded914bac53cc955bd96e665bba2b3f9
minwoo <zox2323@naver.com>
2019-01-27 16:27:55 +0900

    rank update

--- router/rank/index.js
+++ router/rank/index.js
@@ -1,10 +1,18 @@
 var conn = require('../../config/db');
 var router = require('express').Router();
-router.post('/',function(req,res){
+router.get('/',function(req,res){
+/*
   var sql = `select * from (
  select nickname, score, (@r:=@r+1) as ranking from user, (select @r:=0) as B
  order by score, nickname) as T
 where ranking<=10`;
+*/
+var sql =`select nickname, score, score div 500 tier
+from user order by score;`;
+var tier =new Array(6);
+for(var i=0;i<6;i++)
+  tier[i]=new Array();
+
 
   conn.query(sql, [],function(err,result){
     if(err)
@@ -13,8 +21,16 @@ where ranking<=10`;
       var msg = {'status':'ERROR'};
       return res.json(msg);
     }
-    var msg = {'status':'OK','result':result};
-    res.send(msg);
+    for(var i=0;i<result.length;i++)
+    {
+      if(tier[result[i].tier].length>=10)
+        continue;
+      tier[result[i].tier].push(result[i]);
+    }
+    var msg = {'status':'OK','0':tier[0],'1':tier[2],'2':tier[3],'3':tier[4],
+    '4':tier[4],'5':tier[5]}
+    console.log(JSON.stringify(msg));
+    res.json(msg);
   });
 })
 module.exports = router;
