c03d2757f762c83a1523d611ea68718375f09d55
Lee Dong Gil <donggillee94@gmail.com>
2019-01-31 21:34:19 +0900

    initial commit

--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,7 @@ module.exports=function(app){
   var hasher = bkfd2Password();
   var passport = require('passport');
   var LocalStrategy = require('passport-local').Strategy;
+  var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
   app.use(passport.initialize()); //passport초기화!
   app.use(passport.session()); //passport에서 세션을쓰겠다.
 
@@ -19,5 +20,19 @@ module.exports=function(app){
 
     }
   ));
+  var googleCredentials = require('google.json');
+
+  passport.use(new GoogleStrategy({
+      clientID: googleCredentials.web.client_id,
+      clientSecret: googleCredentials.web.client_secret,
+      callbackURL: googleCredentials.web.redirect_uris
+    },
+    function(accessToken, refreshToken, profile, done) {
+      User.findOrCreate({googleId: profile.id}, function(err, user) {
+        return done(err, user);
+      });
+    }
+  ));
+  app.get('google/auth')
   return passport
 }
