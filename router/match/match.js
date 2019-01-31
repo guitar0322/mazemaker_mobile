5950925b3f1c71a4cc3f16d22a0a0ca34051ff27
Lee Dong Gil <donggillee94@gmail.com>
2019-01-31 20:21:11 +0900

    initial commit

--- router/match/match.js
+++ router/match/match.js
@@ -26,7 +26,10 @@ router.post('/',function(req,res){
             if(err) throw err;
             else if(result.length === 2) {
               var msg = {"complete":"COMPLETE", "result":result};
-              return res.json(msg);
+              conn.query('delete from match_que where room = ?', result[0].room, (err, result) => {
+                if(err) throw err;
+                return res.json(msg);
+              })
             }
             else {
               var msg = {"complete":"ERROR"};
