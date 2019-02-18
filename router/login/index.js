a8f3872e28123d9e3086177a36693830d5e7869c
Lee Dong Gil <donggillee94@gmail.com>
2019-02-18 16:31:31 +0900

    initial commit

--- router/login/index.js
+++ router/login/index.js
@@ -27,40 +27,21 @@ router.post('/',function(req,res){
       throw err;
     }
     if(result.length===0){
-      var msg = {"status":"ERROR"};
+      var msg = {"status":"FIRST"};
       return res.json(msg);
     }
+    else {
+      var nickname = result[0].nickname;
+      var win = result[0].win;
+      var lose = result[0].lose;
+      var league = result[0].score;
+      var last_date = result[0].last_date;
+      var ticket = result[0].ticket;
+      var tutorial = result[0].tutorial;
+      var msg = {"status":"OK", "nickname":nickname, "win":win, "lose":lose, "league":league, "ticketchangedtime":last_date, "ticket":ticket, "tutorial":tutorial}
 
-    var user = result[0].username;
-    var pw = result[0].password;
-    var salt =result[0].salt;
-    var nickname = result[0].nickname;
-    var win = result[0].win;
-    var loss = result[0].loss;
-    var score = result[0].score;
-    var last_date = result[0].last_date;
-
-    return hasher({password:req.body.password,salt:salt},
-      function(err,pass,salt,hash){
-        if(hash===pw){
-          var msg={"status":"OK",'nickname':nickname, 'win':win, 'loss':loss, 'score':score, 'last_date':last_date};
-          var tmp = {
-            last_date:req.body.last_date
-          }
-          var arr = new Array();
-          arr[0] = tmp;
-          arr[1] = user;
-          conn.query('update user set ? where username = ?', arr, (err, result) => {
-            if(err) throw err;
-            console.log(last_date);
-            return res.json(msg);
-          })
-        }
-        else{
-          var msg={"status":"ERROR"};
-          return res.json(msg);
-        }
-      });
+      return res.jsonI(msg);
+    }
   });
 });
 module.exports = router;
