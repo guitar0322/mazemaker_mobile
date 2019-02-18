ee6a5adc485235ab4d58c129f828ef0626ef0f2d
minwoo <zox2323@naver.com>
2019-02-18 18:11:02 +0900

    rank complete

--- router/rank/index.js
+++ router/rank/index.js
@@ -1,18 +1,17 @@
 var conn = require('../../config/db');
 var router = require('express').Router();
-router.get('/',function(req,res){
-/*
+router.post('/',function(req,res){
+
   var sql = `select * from (
  select nickname, score, (@r:=@r+1) as ranking from user, (select @r:=0) as B
- order by score, nickname) as T
+ order by score desc, nickname) as T
 where ranking<=10`;
-*/
+
+
+/*
 var sql =`select nickname, score, score div 500 tier
 from user order by score;`;
-var tier =new Array(6);
-for(var i=0;i<6;i++)
-  tier[i]=new Array();
-
+*/
 
   conn.query(sql, [],function(err,result){
     if(err)
@@ -21,16 +20,30 @@ for(var i=0;i<6;i++)
       var msg = {'status':'ERROR'};
       return res.json(msg);
     }
-    for(var i=0;i<result.length;i++)
-    {
-      if(tier[result[i].tier].length>=10)
-        continue;
-      tier[result[i].tier].push(result[i]);
-    }
-    var msg = {'status':'OK','0':tier[0],'1':tier[2],'2':tier[3],'3':tier[4],
-    '4':tier[4],'5':tier[5]}
-    console.log(JSON.stringify(msg));
-    res.json(msg);
+
+    console.log(result);
+    console.log(result[0]);
+    var top=[];
+    for(var i in result)
+      top.push({"nickname":result[i].nickname,"rankscore":result[i].score});
+
+    sql = `With T(nickname, score, ranking) as (select nickname, score, (@r:=@r+1) as ranking from user, (select @r:=0) as B
+    order by score desc, nickname)
+    select nickname, score from T where ranking DIV 11 = (select ranking from T where nickname= ?) DIV 11;`;
+    conn.query(sql,[req.body.nickname],function(err,result){
+      if(err)
+        throw err;
+      if(result.lenght===0){
+        var msg = {'status':'ERROR'};
+        return res.json(msg);
+      }
+      var around = [];
+      for(var i in result)
+        around.push({"nickname":result[i].nickname,"rankscore":result[i].score});
+
+      var msg = {"top":top, "around":around};
+      return res.json(msg);
+    })
   });
 })
 module.exports = router;
