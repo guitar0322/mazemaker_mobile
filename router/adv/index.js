eff3595e50fb5581b950e101fd46aba35af06df6
Lee Dong Gil <donggillee94@gmail.com>
2019-02-23 16:07:18 +0900

    initial commit

--- router/adv/index.js
+++ router/adv/index.js
@@ -4,7 +4,10 @@ var router = require('express').Router();
 router.post('/', (req, res) =>{
   var nickname = req.body.nickname;
   console.log('advertisement : ', nickname);
-  conn.query()
+  conn.query('update user set ticket = ticket + 1 where nickname = ?', nickname, (err, result) => {
+    if(err) throw err;
+
+  })
 })
 
 module.exports = router;
