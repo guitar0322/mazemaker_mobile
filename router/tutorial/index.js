4cffbaa7754951c934d917fbafa29932635d6778
donggillee94 <donggillee94@gmail.com>
2019-02-27 21:58:12 +0900

    initial commit

--- router/tutorial/index.js
+++ router/tutorial/index.js
@@ -3,7 +3,7 @@ var router = require('express').Router();
 
 router.post('/', (req, res) =>{
   var nickname = req.body.nickname;
-
+  console.log("tutorial complete : ", nickname);
   conn.query('select * from user where nickname = ?', nickname, (err, result) => {
     if(err) throw err;
     conn.query('update user set tutorial = 1 where nickname = ?', nickname, (err, result) => {
