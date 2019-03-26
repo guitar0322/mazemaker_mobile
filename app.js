c2155a75eee860f7a0aa21c2c36fcb20e72c19fc
Lee Dong Gil <donggillee94@gmail.com>
2019-03-26 20:07:18 +0900

    initial commit

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ var app = require('./config/express')();
 var router = require('./router/index');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var conn = require('./config/db')
-conn.connect();
-conn.on('error', function() {});
+
 require('./socket/ingame_socket')(io,conn);
 require('./socket/match_socket')(io,conn);
 
