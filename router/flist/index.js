e73a49ab487c328d759976e016f32e81eafa0254
System Administrator <root@idong-gil-ui-MacBook-Pro.local>
2019-05-09 11:07:16 +0900

    modified Pool.connection & friend_list

---
+++ router/flist/index.js
@@ -0,0 +1,40 @@
+var router = require('express').Router();
+var pool = require('../../config/db')
+
+router.post('/', function(req, res) {
+  var sql = "select * from Friend where sender = ? and relation = ?";
+
+  pool.getConnection((err, connection) => {
+    connection.query(sql, [req.body.nickname, 0], function(err, result) {
+      if (err) {
+				connection.release();
+				throw err
+			}
+      if(result.length === 0) {
+        var msg = {"result":"NONE"};
+        res.json(msg);
+      }
+      else {
+        var sql = `select u.nickname, u.status
+        from Friend as f, user as u
+        where f.receiver=u.nickname and f.sender=? and f.relation = ?;`
+        connection.query(sql, [req.body.nickname, 1], function(err, result) {
+          if(err) {
+            connection.release();
+            throw err;
+          }
+          var f_list = [];
+
+          for(var i in result) {
+            f_list.push({"nickname":result[i].nickname, "status":result[i].status});
+          }
+          var msg = {"result":f_list};
+          res.json(msg);
+        })
+      }
+    })
+    connection.release();
+  })
+})
+
+module.exports = router;
